ec36dd34f33a744edd5a3be4f6ac21daeee61f26
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-09-19 13:06:22 -0500

    initial styles definition

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,102 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  static const titleColor = Color(0xFF000AFF);
+  static const backgroundColor = Color(0xFFFFFFFF);
+  static const onBackgroundColor = Color(0xFF000000);
+  static const primaryColor = Color(0xFF002093);
+  static const onPrimaryColor = Color(0xFFFFFFFF);
+  static const secondaryColor = Color(0xFFFDD140);
+  static const onSecondaryColor = Color(0xFF000000);
+  static const surfaceColor = Color(0xFFF0F0F0);
+  static const onSurfaceColor = Color(0xFFA4A4A4);
+  static const errorColor = Color(0xFFB00020);
+  static const onErrorColor = Color(0xFFFFFFFF);
+  static const tertiaryColor = Color(0xFFD9D9D9);
+  static const onTertiaryColor = Color(0xFF000000);
+  static const outlineColor = Color(0xFF000000);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Renters.io',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light, // modo claro
+          background: backgroundColor, // color de fondo
+          onBackground: onBackgroundColor, // color de texto en el fondo
+          primary: primaryColor, // color de navbar
+          onPrimary: onPrimaryColor, // color de texto en navbar
+          secondary: secondaryColor, // color de botones
+          onSecondary: onSecondaryColor, // color de texto en botones
+          surface: surfaceColor, // color de fondo de los inputs
+          onSurface: onSurfaceColor, // color de texto en los inputs 
+          error: errorColor, // color de error
+          onError: onErrorColor, // color de texto en error
+          tertiary: tertiaryColor, // color de fondo de los productos y notificaciones
+          onTertiary: onTertiaryColor, // color de texto en los productos y notificaciones
+          outline: outlineColor, // color de bordes
+        ),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle( // titulo de la app
+            color: titleColor,
+            fontSize: 64,
+            fontWeight: FontWeight.w900,
+            fontFamily: 'Lato',
+          ),
+          displayMedium: TextStyle( // Agrega un emprendimiento
+            color: onBackgroundColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Poppins',
+          ),
+          titleMedium: TextStyle( // Campos requeridos para llenar
+            color: onBackgroundColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            fontFamily: 'Lato',
+          ),
+          bodyLarge: TextStyle( // costo en las notificaciones
+            color: onBackgroundColor,
+            fontSize: 18,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Lato', 
+          ),
+          bodyMedium: TextStyle( // titulo de las notificaciones
+            color: onBackgroundColor,
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Lato',
+          ),
+          bodySmall: TextStyle( // texto de las notificaciones
+            color: onBackgroundColor,
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Lato',
+          ),
+          labelLarge: TextStyle(// Botones
+            color: onSecondaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            fontFamily: 'Poppins',
+          ),
+          labelMedium: TextStyle( // Botones de filtros
+            color: onSecondaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            fontFamily: 'Poppins',
+          ),
+          labelSmall: TextStyle( // Texto dentro de inputs
+            color: onSurfaceColor,
+            fontSize: 13,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Lato',
+          )
+          
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const Text('Hello, world!'),
     );
   }
 }
